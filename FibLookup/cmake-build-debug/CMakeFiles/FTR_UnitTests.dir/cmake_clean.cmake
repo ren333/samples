b780c6392a4a997a7ac6f720da99deda2eb935b3
Renato <renk333@gmail.com>
2018-04-30 18:21:36 +1000

    Added fibonacci lookup application, uses file for list of fib numbers

---
+++ FibLookup/cmake-build-debug/CMakeFiles/FTR_UnitTests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FTR_UnitTests.dir/FTR_CodingTest_UnitTests.cpp.o"
+  "CMakeFiles/FTR_UnitTests.dir/FrequencyTicker.cpp.o"
+  "FTR_UnitTests.pdb"
+  "FTR_UnitTests"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FTR_UnitTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
